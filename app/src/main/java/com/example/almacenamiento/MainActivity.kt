28614936261e3d169e9fc457a1d47e6135f426bd
martinedgar711-lgtm <rafa_huerta@comunidad.unam.mx>
2026-06-01 21:14:56 -0600

    ejercicio 4

--- app/src/main/java/com/example/almacenamiento/MainActivity.kt
+++ app/src/main/java/com/example/almacenamiento/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.almacenamiento
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -31,8 +32,10 @@ class MainActivity : AppCompatActivity() {
         val filename = "datos_usuario.txt"
         val fileContents = "Puntaje: 1500\nNivel: 5"
 
-        openFileOutput(filename,
-            Context.MODE_PRIVATE).use { output ->
+        openFileOutput(
+            filename,
+            Context.MODE_PRIVATE
+        ).use { output ->
             output.write(fileContents.toByteArray())
         }
 
@@ -40,6 +43,11 @@ class MainActivity : AppCompatActivity() {
         Reader().use { reader ->
             val text = reader.readText()
             binding.tvwContentFile.txt = text
-
         }
+
+        val arrayArchivos: Array<String> = fileList()
+        arrayArchivos.forEach { nombre ->
+            Log.i("Stored", nombre)
+        }
+    }
 }
